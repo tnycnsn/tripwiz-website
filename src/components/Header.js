a49389038fa3817b1f68fa789c2610e6eee0115c
Tanay Cansin Dogan <tnycnsn@gmail.com>
2025-06-21 15:36:28 +0200

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="header-container">
         <Link to="/" className="logo-container">
           <img 
-            src="/tripwiz.travel/logo192.png" 
+            src="/logo192.png"
             alt="TripWiz Logo" 
             className="logo"
           />
